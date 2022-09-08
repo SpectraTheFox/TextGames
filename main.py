abb693dc806ac847d3fd17bc7583a2d0d76c4b0b
SpectralWolf <69986315+SpectralWolf590@users.noreply.github.com>
2022-09-08 19:57:04 +0000

    Added Files

---
+++ main.py
@@ -0,0 +1,11 @@
+#NO SCREW WITH DIS
+import pickle
+import time
+import os
+import random, sys
+if len(sys.argv) - 1:
+    random.seed(int(sys.argv[1]))
+
+
+
+print("Would You like to play a game?")
