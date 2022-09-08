30227d09e21c9c1177a3cac49649ceec954dcb1b
SpectraTheFox <69986315+SpectraTheFox@users.noreply.github.com>
2022-09-08 20:50:20 +0000

    Started main.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# TextGames
-A Bunch Of text games i made
+# Explination
+A Bunch Of text games I made
+Use "Python3 {game name.py}" to play any game from the list below
+
+# GAME LIST
+main.py (A super random text game, its pretty wierd)
--- main.py
+++ main.py
@@ -6,6 +6,29 @@ import random, sys
 if len(sys.argv) - 1:
     random.seed(int(sys.argv[1]))
 
-
+#######################################
+loadingrepeat = 0
+def loadingscreen(loadingrepeat):
+  loading = "Loading"
+  while loadingrepeat != 0:
+    
+    dottimer = 4
+    while dottimer != 0:
+      print(loading)
+      time.sleep(1)
+      loading += "."
+      dottimer -= 1
+      
+    loadingrepeat -= 1
+    loading = "Loading"
 
 print("Would You like to play a game?")
+answer = input(": ")
+if answer in ["yes", "Yes", "No", "no"]:
+  if answer in ["no", "No"]:
+    print("goodbye then")
+    exit()
+  elif answer in ["yes", "Yes"]:
+    print("Let us Continue...")
+    loadingscreen(2)
+  
